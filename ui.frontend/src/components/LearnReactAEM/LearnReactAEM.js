4a812b189717662c3b89c557da7b4f998428398e
Vaishnav S <vaishnav.s@cognizant.com>
2022-04-01 19:28:32 +0530

    Fetching external APIs inside React component js class

--- ui.frontend/src/components/LearnReactAEM/LearnReactAEM.js
+++ ui.frontend/src/components/LearnReactAEM/LearnReactAEM.js
@@ -13,6 +13,11 @@ export const LearnReactAEMConfig = {
 export default class LearnReactAEM extends Component {
 
     render() {
+
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json));
+
         if(LearnReactAEMConfig.isEmpty(this.props)) {
             return null;
         }
